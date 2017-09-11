b2063104f3698b092294ccf03d668404139f3f09
Brian Henderson <brian.henderson1@marist.edu>
2017-09-11 09:28:08 -0400

    python version script and terminal proof

---
+++ henderson-00/versions.py
@@ -0,0 +1,17 @@
+#versions.py
+
+import sys
+import numpy
+import scipy
+import sklearn
+import matplotlib
+import pandas
+import tensorflow
+
+print(sys.version)
+print(numpy.__version__)
+print(scipy.__version__)
+print(sklearn.__version__)
+print(matplotlib.__version__)
+print(pandas.__version__)
+print(tensorflow.__version__)
